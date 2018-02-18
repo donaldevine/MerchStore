2306240ccd7d4c5fdcea0d5c4140fbe7d3944e92
Donal Devine <donaldevine@gmail.com>
2018-02-18 21:32:51 +0000

    Added MVC 6 stuff

---
+++ MerchStore/Controllers/AppController.cs
@@ -0,0 +1,51 @@
+﻿using MerchStore.Services;
+using MerchStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MerchStore.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email    
+                this.mailService.SendMessage("test@testing.test", model.Subject, model.Message);                
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ MerchStore/Services/NullMailService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MerchStore.Services
+{
+    public class NullMailService : IMailService
+    {
+        private readonly ILogger<NullMailService> _logger;
+
+        public void SendMessage(string to, string subject, string body)
+        {
+            _logger.LogInformation($"To: {to}. Subject: {subject}. body: {body}.");
+        }
+
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            
+        }
+    }
+}
+
+
